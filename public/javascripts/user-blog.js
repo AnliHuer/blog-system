e29ef0f1a3d19fe4a079668df69d06b0f0a951c0
anlihuer <anlihuer@outlook.com>
2016-06-02 19:11:47 +0800

    delete add-tag button in views and create function in js

--- public/javascripts/user-blog.js
+++ public/javascripts/user-blog.js
@@ -1,16 +1,21 @@
-$(function () {
+function getBlogByTag() {
     $('#blog-tag').on('click', 'a', function () {
         $.get('/user-tag', {blogTagId: this.id}, function (resp) {
             var string = '';
 
             $('#blogs ul').remove();
-            resp.blogDetails.forEach(function(val){
-                string +='<'+'a href="user-blog-detail?id="'+val.id +' class="blog-name">'+val.blogName+'</a>'+
-                    '<'+'li class="blog-createdAt">'+val.createdAt+'</li>'+
-                    '<'+'div class="blog-line"></div>';
+            resp.blogDetails.forEach(function (val) {
+                string += '<' + 'a href="user-blog-detail?id="' + val.id + ' class="blog-name">' + val.blogName + '</a>' +
+                    '<' + 'li class="blog-createdAt">' + val.createdAt + '</li>' +
+                    '<' + 'div class="blog-line"></div>';
             })
 
-            $('#blogs').append('<'+'ul>'+string+'</ul>');
+            $('#blogs').append('<' + 'ul>' + string + '</ul>');
         });
     });
+}
+
+
+$(function () {
+    getBlogByTag();
 })
