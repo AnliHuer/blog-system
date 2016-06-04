d35c4ea9fa01fe14073cd0e22f8b65e95b740e15
anlihuer <anlihuer@outlook.com>
2016-06-04 14:56:20 +0800

    get all blog tags when this user create new tag

--- controllers/user-new-blog-controller.js
+++ controllers/user-new-blog-controller.js
@@ -1,12 +1,40 @@
 var models = require('../models');
-var CONSTANT = require('../public/javascripts/all-constant.js');
+var buildBlogTags = require('./buildBlogTags');
 var User = models.User;
+var BlogTag = models.BlogTag;
+var BlogDetail = models.BlogDetail;
 
 function UserNewBlogController() {
 }
 
 UserNewBlogController.prototype.displayPage = function (req, res) {
-    res.render('user-new-blog',{user: req.cookies});
+    var userId = req.cookies.userId;
+    var blogList = [];
+
+    User.findOne({
+        where: {
+            id: userId
+        }
+    }).then(function (user) {
+        BlogDetail.findAll({
+            where: {
+                userId: userId
+            }
+        }).then(function (blogDetails) {
+            blogDetails.forEach(function (blogDetail) {
+                BlogTag.findOne({
+                    where: {
+                        id: blogDetail.blogTagId
+                    }
+                }).then(function (blogTag) {
+                    blogList.push({blogDetail: blogDetail, blogTag: blogTag});
+                })
+            });
+            setTimeout(function () {
+                res.render('user-new-blog', {user: user, blogTags: buildBlogTags(blogList)});
+            }, 100);
+        });
+    });
 }
 
 
